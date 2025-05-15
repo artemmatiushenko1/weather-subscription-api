ea7edd3f744dca1bbbafb9c3223b1dd6d3939062
artemmatiushenko1 <artom.matyushenko@gmail.com>
2025-05-15 20:33:53 +0300

    add required modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { WeatherModule } from './weather/weather.module';
+import { SubscriptionModule } from './subscription/subscription.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [WeatherModule, SubscriptionModule, EmailModule],
 })
 export class AppModule {}
